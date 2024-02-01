5941a5b5a25a0482ea8c5aad8aea0a664c4875e1
Andrii Bakanov <andriibakanov@gmail.com>
2024-02-01 23:32:23 +0100

    no message

--- components/Contacts/Contacts.js
+++ components/Contacts/Contacts.js
@@ -7,7 +7,7 @@ const Contacts = () => {
         <address className={s.contactsContainer}>
                   <MobileSvg />
                   <div className={s.telContainer}>
-                      <a href="tel:+380442055977">+38 044 205 5977</a>
+                      <a href="tel:+380442055977">+380504111721</a>
                       <button className={s.telBtn}>обратный звонок</button>
                       <button className={s.telBtn}>показать все номера</button>
                   </div>
