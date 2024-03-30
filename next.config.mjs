b431c36d4ee4d5df486284328c2aee236e66469c
Andrii Bakanov <andriibakanov@gmail.com>
2024-03-30 14:05:50 +0100

    no message

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,4 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-/* multilanguage
-const { nextI18NextRewrites } = require('next-i18next/rewrites')
-
-const localeSubPaths = {
-  en: 'en'
-}
-
-module.exports = {
-  rewrites: async () => nextI18NextRewrites(localeSubPaths),
-}
-
-*/
-
 export default nextConfig;
