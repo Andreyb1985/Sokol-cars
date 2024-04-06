fd371bdd99b59af1ed35cb992812071e1f4a6aca
Alexandr Kotliar <akotliar@unisender.com>
2024-04-06 23:28:52 +0200

    no message

--- src/config.js
+++ src/config.js
@@ -2,6 +2,11 @@ export const locales = ['en', 'ru', 'de'];
 
 export const pathnames = {
   '/': '/',
+  '/company/about-us': {
+    en: '/test',
+    ru: '/test',
+    de: '/test'
+  }
 };
 
 // Use the default: `always`
--- src/middleware.js
+++ src/middleware.js
@@ -1,14 +1,19 @@
 import createMiddleware from 'next-intl/middleware';
- 
+
 export default createMiddleware({
   // A list of all locales that are supported
   locales: ['en', 'ru', 'de'],
- 
+
   // Used when no locale matches
-  defaultLocale: 'ru'
+  defaultLocale: 'ru',
+  localePrefix: 'always',
 });
- 
+
 export const config = {
   // Match only internationalized pathnames
-  matcher: ['/', '/(ru|en|de)/:path*']
+  matcher: [
+    '/',
+    '/(en|ru|de)/:path*',
+    '/((?!_next|_vercel|_next|.*\\..*).*)'
+  ],
 };
